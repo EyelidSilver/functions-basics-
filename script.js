2a9f0654a3a25b8bd997540fccde7ee73cf13bc9
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2021-03-02 11:38:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const makePizza = () => {
+    console.log("make a pizza bottom")
+    console.log("make tomato sauce")
+    console.log("add sauce to the pizza bottom")
+    console.log("chop vegetables")
+    console.log("add vegetables to the sauced pizza bottom")
+    console.log("bake the pizza")
+}
+
+
+const makeSushi = () => {
+    console.log("boil water")
+    console.log("add rice")
+    console.log("drain rice")
+    console.log("add rice to nori")
+    console.log("roll nori and rice")
+    console.log("chop nori and rice roll")
+}
+
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
